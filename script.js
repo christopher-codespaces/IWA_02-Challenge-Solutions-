396d6c81367b1664f239e37d2c2ef94c423ba1ef
Christopher Makombe <125880140+christopher-codespaces@users.noreply.github.com>
2023-03-29 07:08:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//Challenge 1 syntax error
+// This is the firstname of the user
+const user = 'John';
+
+// This is the lastname of the user
+const surname = 'Smith';
+
+console.log(user, surname);
+
+
+// Challenge 2  typo in the last line. 
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014';
+
+console.log(date);
+
+//Challenge 3 Typo in console
+console.warn('Security scan starting');
+
+/* It is important to let users know when they can close the page */
+console.info('Please wait for scan to complete before closing the browser.');
+
